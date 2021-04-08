116ecc07fa86b63f09c9cdb4f53253c095e35fb3
momnus <momnus@ya.ru>
2021-04-08 13:46:51 +0300

    Beat-Marker System

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    private FMOD.Studio.EventInstance instance;
+    private BeatSystem bS;
+
+    void Start()
+    {
+        bS = GetComponent<BeatSystem>();     
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            instance = FMODUnity.RuntimeManager.CreateInstance("event:/music");
+            instance.start();
+            bS.AssignBeatEvent(instance);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            bS.StopAndClear(instance);
+        }
+    }
+}
---
+++ Assets/Scripts/UpdateText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UpdateText : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text beat, marker;
+
+    void Update()
+    {
+        beat.SetText("Beat: " + BeatSystem.beat);
+        marker.SetText("Marker: " + BeatSystem.marker);
+    }
+}
